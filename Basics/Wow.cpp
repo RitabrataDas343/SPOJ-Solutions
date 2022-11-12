16116b9d6b10105df8e129dfdf1183b98b106520
Ritabrata Das <dasritabrata5@gmail.com>
2022-11-12 18:11:26 +0530

    defined folder structure

---
+++ Basics/Wow.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string m = "W";
+    while(n--){
+        m += 'o';
+    }
+    m += 'w';
+    cout << m << endl;
+    return 0;
+}
