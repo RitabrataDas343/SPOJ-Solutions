b025139fdca70ee7735231ac277374cfce872682
Ritabrata Das <dasritabrata5@gmail.com>
2022-11-12 18:15:16 +0530

    defined folder structure

---
+++ Basics/Sum of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    float a, b;
+    cin >> n;
+    while(n--){
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
